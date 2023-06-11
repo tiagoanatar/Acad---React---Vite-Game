eb1dc2fa9c53ee8f7582ee841af816bbeb3b30b9
tiagoanatar <tiagoanatar@gmail.com>
2023-06-11 18:07:09 -0300

    Add

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+
 export const Main = () => {
+
+  // Prevent page refresh with the G5 by mistake
+  // useEffect(() => {
+  //   const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+  //     event.preventDefault();
+  //     event.returnValue = ''; // This line is necessary for Chrome compatibility
+  //   };
+
+  //   window.addEventListener('beforeunload', handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener('beforeunload', handleBeforeUnload);
+  //   };
+  // }, []);
+
   return (
     <>
       <div>Hello</div>
